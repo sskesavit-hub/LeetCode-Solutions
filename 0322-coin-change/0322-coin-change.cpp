bca582ec60074564f30131b521c1cb52989553ed
KESEVAN S <sskesav.it@gmail.com>
2026-09-09 14:40:16 +0530

    Time: 45 ms (38.75%), Space: 53 MB (12.37%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,25 @@
+class Solution { 
+public: 
+    int coinChange(vector<int>& coin, int amount) { 
+        int n = coin.size(); 
+        int m = amount; 
+        
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0)); 
+        
+        for(int i = 0; i <= m; i++){ 
+            dp[0][i] = 1e9; 
+        } 
+
+        for(int i = 1; i <= n; i++){ 
+            for(int j = 1; j <= m; j++){ 
+                if(coin[i-1] > j){ 
+                    dp[i][j] = dp[i-1][j]; 
+                } 
+                else{ 
+                    dp[i][j] = min(dp[i-1][j], 1 + dp[i][j-coin[i-1]]); 
+                } 
+            } 
+        } 
+        return dp[n][m] >= 1e9 ? -1 : dp[n][m]; 
+    } 
+};
